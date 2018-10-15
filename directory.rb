cd107404ba03ae30d8d885919b88d9456a0aafe0
Max <maxstevenson@msn.com>
2018-10-15 09:16:52 +0100

    Created method for user to input custom names into student hash

--- directory.rb
+++ directory.rb
@@ -13,6 +13,22 @@ students = [
 {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	while !name.empty? do
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} students"
+		name = gets.chomp
+	end
+	# return array of students
+	students
+end
+
 # Header method to print beginner header
 def print_header
 	puts "The students of Villians Academy"
@@ -31,6 +47,7 @@ def print_footer student_arr
 	puts "Overall, we have #{student_arr.count} great students"
 end
 
+students = input_students
 print_header
 print_students (students)
 print_footer (students)
